a9e764cc8255e211e001173650ab970c8d967fcf
Jiří Meitner <jiri001meitner@gmail.com>
2014-07-05 12:44:27 +0200

    So čec  5 12:44:27 CEST 2014

--- refreshblocking.sh
+++ refreshblocking.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo mv /etc/hosts.d/temporarily-disabled /etc/hosts && echo -e "Temporary suspension has been lifted. Edithosts is activ"
+sudo mv /etc/hosts.d/temporarily-disabled /etc/hosts && echo -e "Temporary suspension has been lifted. Edithosts is active"
 exit 0
