2c90c4a57d82f5e1375db86642ad67b0f130b017
Jiří Meitner <jiri001meitner@gmail.com>
2014-07-07 01:50:05 +0200

    Po čec  7 01:50:05 CEST 2014

--- gethosts.sh
+++ gethosts.sh
@@ -13,9 +13,7 @@ wget -nv -O - "http://hosts-file.net/ad_servers.asp" >> "$temphosts1"
 wget -nv -O - "http://someonewhocares.org/hosts/hosts" >> "$temphosts1"
 wget -nv -O - "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext" >> "$temphosts1"
 wget -nv -O - "https://raw.githubusercontent.com/jiri001meitner/edithosts/master/edithosts-blocklist.txt" >> "$temphosts1"
-
 sed -e 's/\r//' -e '/^127\.0\.0\.1/!d' -e '/localhost/d' -e 's/127\.0\.0\.1/0.0.0.0/' -e 's/ \+/\t/' -e 's/#.*$//' -e 's/[ \t]*$//' < "$temphosts1" | sort -u > "$temphosts2"
-
 echo -e "\n# Edithost updated this file at $(date)" | cat /etc/hosts.d/hosts.conf - "$temphosts2" > /etc/hosts.d/hosts-block
 echo -e "\n# Blocked $(cat /etc/hosts.d/hosts-block | grep 0.0.0.0 | wc -w) domains" >> /etc/hosts.d/hosts-block
 rm "$temphosts1" "$temphosts2"
