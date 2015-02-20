975133a375b8a10d68dfc7b1fdaa3f6d55398236
jiri001meitner <jiri001meitner@gmail.com>
2015-02-20 04:02:57 +0100

    Pá úno 20 04:02:57 CET 2015

--- uninstall.sh
+++ uninstall.sh
@@ -6,5 +6,6 @@ sudo mv /etc/hosts.d/hosts.bak /etc/hosts
 sudo rm /etc/hosts.d/ -rf
 sudo rm /etc/cron.monthly/updateprogramuedithosts
 sudo rm /etc/cron.weekly/edithosts
+sudo rm /usr/share/man/cs/man1/edithosts.1.gz
 echo "edithost: Uninstall OK"
 exit 0
--- update.sh
+++ update.sh
@@ -11,6 +11,8 @@ sudo cp -f /tmp/edithosts/uninstall.sh /opt/edithosts/uninstall.sh
 sudo cp -f /tmp/edithosts/licence /opt/edithosts/licence
 sudo cp -f /tmp/edithosts/temporarilydisableblocking.sh /opt/edithosts/temporarilydisableblocking.sh
 sudo cp -f /tmp/edithosts/refreshblocking.sh /opt/edithosts/refreshblocking.sh
+sudo gzip -c /tmp/edithosts/man/cs/man1/edithosts.1 > man/cs/man1/edithosts.1.gz
+sudo cp -f /tmp/edithosts/man/cs/man1/edithosts.1.gz /usr/share/man/cs/man1/edithosts.1.gz
 
 sudo chmod 755 /opt/edithosts -R
 
