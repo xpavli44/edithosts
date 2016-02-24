76f1220696faaaceb79fc1a8720e1c10572c42ab
jiri001meitner <jiri001meitner@gmail.com>
2016-02-25 00:00:09 +0100

    Čt úno 25 00:00:09 CET 2016

--- edithosts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo $EDITOR /etc/hosts.d/hosts.conf && sudo /opt/edithosts/gethosts.sh || sudo nano /etc/hosts.d/hosts.conf && sudo /opt/edithosts/gethosts.sh
-
-exit 0
--- refreshblocking.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo mv /etc/hosts.d/temporarily-disabled /etc/hosts && echo -e "Temporary suspension has been lifted. Edithosts is active"
-exit 0
