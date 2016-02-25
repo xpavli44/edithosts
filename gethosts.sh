9b378882cc855b5514aa542535931c0b23275df0
jiri001meitner <jiri001meitner@gmail.com>
2016-02-25 01:41:34 +0100

    Čt úno 25 01:41:34 CET 2016

---
+++ gethosts.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo "edithosts:Checking Internet connectivity..."
+	if [ "`ping -c 1 raw.githubusercontent.com`" ]
+then
+
+  echo "edithosts:Internet connectivity OK..."
+
+temphosts1a=$(mktemp)
+temphosts1b=$(mktemp)
+temphosts2a=$(mktemp)
+temphosts2b=$(mktemp)
+temphosts3=$(mktemp)
+whitelist=$(mktemp)
+
+wget -nv -O - "https://raw.githubusercontent.com/jiri001meitner/edithosts/master/edithosts-blocklist.txt" >> "$temphosts1a"
+wget -nv -O - "http://winhelp2002.mvps.org/hosts.txt" >> "$temphosts1a"
+wget -nv -O - "http://hosts-file.net/ad_servers.asp" >> "$temphosts1b"
+wget -nv -O - "http://someonewhocares.org/hosts/hosts" >> "$temphosts1b"
+wget -nv -O - "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext" >> "$temphosts1b"
+wget -nv -O - "https://raw.githubusercontent.com/Free-Software-for-Android/AdAway/master/hosts/hosts.txt" >> "$temphosts1b"
+
+sed -e 's/\r//' -e '/^0.0.0.0/!d' -e '/localhost/d' -e 's/127\.0\.0\.1/0.0.0.0/' -e 's/ \+/\t/' -e 's/#.*$//' -e 's/[ \t]*$//' < "$temphosts1a" | sort -u > "$temphosts2a"
+
+sed -e 's/\r//' -e '/^127\.0\.0\.1/!d' -e '/localhost/d' -e 's/127\.0\.0\.1/0.0.0.0/' -e 's/ \+/\t/' -e 's/#.*$//' -e 's/[ \t]*$//' < "$temphosts1b" | sort -u > "$temphosts2b"
+
+cat "$temphosts2a" "$temphosts2b" | sort -u > "$temphosts3"
+
+wget -nv -O - https://raw.githubusercontent.com/jiri001meitner/edithosts/master/whitelist.txt >> "$whitelist"
+
+for patern in $(cat "$whitelist"); do sed -i "/\b$patern\b/Id" "$temphosts3"; done
+
+echo -e "\n# Edithost updated this file at $(date)" | cat "/home/medved/Dropbox/Linux/edithosts-adway/hosts.conf" - "$temphosts3" > "/home/medved/Dropbox/Linux/edithosts-adway/hosts"
+echo -e "\n# Blocked $(cat "/home/medved/Dropbox/Linux/edithosts-adway/hosts" | grep 0.0.0.0 | wc -w) domains" >> "/home/medved/Dropbox/Linux/edithosts-adway/hosts"
+rm "$temphosts1a" "$temphosts1b" "$temphosts2a" "$temphosts2b" "$temphosts3" "$whitelist"
+
+echo "edithosts:Filters are up to date now."
+
+else
+  echo "edithosts:Internet connectivity is not OK, exit."
+fi
+
+exit 0
