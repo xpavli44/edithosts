f5ed11c65efe24231913ce26f00174ba4864315c
Jiří Meitner <jiri001meitner@users.noreply.github.com>
2016-02-25 17:33:29 +0100

    ipv6 ads blocking support

--- gethosts.sh
+++ gethosts.sh
@@ -37,7 +37,7 @@ cat "$temphosts3" "$temphostsipv6" | sort -u > "$temphostsipv4ipv6"
 
 echo -e "\n# Edithost updated this file at $(date)" | sudo cat /etc/hosts.d/hosts.conf - "$temphostsipv4ipv6" > /tmp/edithosts-hosts-block
 echo -e "\n# Blocked $(grep "0.0.0.0" /tmp/edithosts-hosts-block| wc -l) ipv4 and ipv6 domains" >> /tmp/edithosts-hosts-block
-rm "$temphosts1a" "$temphosts1b" "$temphosts2a" "$temphosts2b" "$temphosts3" "$whitelist"
+rm "$temphosts1a" "$temphosts1b" "$temphosts2a" "$temphosts2b" "$temphosts3" "$whitelist" "$temphostsipv6" "$temphostsipv4ipv6"
 sudo cp /tmp/edithosts-hosts-block /etc/hosts
 
 echo "edithosts:Filters are up to date now."
